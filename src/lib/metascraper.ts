2f62d046dc462c729a41d4d9a3360cdd182716d9
cilvia333 <cilvia333@iMac.local>
2020-06-29 13:21:16 +0900

    Add debug code

--- src/lib/metascraper.ts
+++ src/lib/metascraper.ts
@@ -6,9 +6,12 @@ const getOgp = async (url: string) => {
     try {
         const html = await ky.get(url).text();
         const doc = domino.createWindow(html).document;
+        console.log(url);
+        console.log(doc);
         return getMetadata(doc, url); 
     }
     catch(e){
+        console.log(`ERROR: ${e}`);
         return null;
     }
 }
